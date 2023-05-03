7355d1f6d610275d3732e431c970c265f638f1bd
p4l0mita <marcojosuepalomo@gmail.com>
2023-05-03 12:32:02 -0600

    Version 1

---
+++ ejercicio 1 TS/ejercicio1.js
@@ -0,0 +1,8 @@
+"use strict";
+//variables implicitas
+let nombre = "Josue Palomo";
+let dirección = "Cantel, Quetzaltenango";
+let telefono = 27476548;
+let correo = "marcojosuepalomo@gmail.com";
+let estatura = 1.7;
+console.log("Nombre:  ", nombre);
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
+interface tarjeta{
+  titulo: string;
+subtitulos: string;
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +11,52 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Ejercicio1';
+}
+
+@Component({
+selector:'app-root',
+templateUrl'./app.component.html',
+styleUrls [ './app.component.css' ]
+})
+
+
+export class AppcComponent implements OnInit {
+
+  title= 'Mi primer app de angular'
+  publicArregloTarjeta : tarjeta []  []; //variable de tipo arreglo
+
+
+  ngOnInit(): void { //iniciar cuando se ejecuta la pagina
+    this.ArregloTarjeta= [
+      {titulo: 'Video 1, subtitulo: 'Subtitulo de Video'},
+      {titulo: 'Video 2, subtitulo: 'Subtitulo de Video'},
+      {titulo: 'Video 3, subtitulo: 'Subtitulo de Video'},
+    ]
+    
+  }
+
+}
+
+import { Component, OnInit } from '@angular/core';
+
+//Crear una interfaz
+interface Tarjeta{
+  titulo: string;
+  subtitulo: string;
+  num?: number;
+}
+
+export class AppComponent implements OnInit {
+
+  title= 'Mi primer app en angular'
+  public ArregloTarjeta : Tarjeta []= []; //variable de tipo arreglo
+
+  ngOnInit(): void { // Es un metodo de tipo void que se inicializa cuando se ejecuta la pagina
+    this.ArregloTarjeta= [ //cargamos datos al arreglo
+    {titulo: 'Video 1, subtitulo: 'Subtitulo de Video', num: 1},
+    {titulo: 'Video 2, subtitulo: 'Subtitulo de Video', num: 2},
+    {titulo: 'Video 3, subtitulo: 'Subtitulo de Video', num: 3},
+  ]
+    
+  }
 }
